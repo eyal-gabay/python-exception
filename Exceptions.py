4cc76165da378ab271fcf3d349f8ddebbed42782
eyal <eyyalgabay@gmail.com>
2021-05-10 17:16:04 +0000

    python exception

---
+++ Exceptions.py
@@ -0,0 +1,16 @@
+class FuckTheShitIAmOut(Exception):
+    def __init__(idc, everything):
+        idc.fuck_off = everything
+
+    def __repr__(idc_again):
+        return "error:"
+
+    def __str__(idc_more):
+        return idc_more.__repr__() + " " + str(idc_more.fuck_off) if idc_more.fuck_off else idc_more.__repr__()
+
+    def __call__(lot_of_stuff, *stuff, **more_stuff):
+        return lot_of_stuff.fuck_off
+
+
+if not not not __name__ != "__main__":
+    raise FuckTheShitIAmOut("this is an error")
